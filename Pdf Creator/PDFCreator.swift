bc092630f26ec6e93c04c98222cc31e0f9eb7c61
Jon <mog36746@gmail.com>
2020-03-12 15:26:17 +0600

    // update formate and size

--- Pdf Creator/PDFCreator.swift
+++ Pdf Creator/PDFCreator.swift
@@ -44,22 +44,22 @@ class PDFCreator: NSObject {
       // 5
       context.beginPage()
       // 6
-      let titleBottom = addTitle(pageRect: pageRect)
+      let titleBottom = addDate(pageRect: pageRect)
       let subtitleBottom = addSubTitle(pageRect: pageRect, titleTop: titleBottom )
       let imageBottom = addImage(pageRect: pageRect, imageTop: subtitleBottom + 18.0)
       addBodyText(pageRect: pageRect, textTop: imageBottom + 18.0)
       
-      let context = context.cgContext
-      drawTearOffs(context, pageRect: pageRect, tearOffY: pageRect.height * 4.0 / 5.0, numberTabs: 8)
-      drawContactLabels(context, pageRect: pageRect, numberTabs: 8)
+//      let context = context.cgContext
+//      drawTearOffs(context, pageRect: pageRect, tearOffY: pageRect.height * 4.0 / 5.0, numberTabs: 8)
+//      drawContactLabels(context, pageRect: pageRect, numberTabs: 8)
     }
     
     return data
   }
   
-  func addTitle(pageRect: CGRect) -> CGFloat {
+  func addDate(pageRect: CGRect) -> CGFloat {
     // 1
-    let titleFont = UIFont.systemFont(ofSize: 18.0, weight: .bold)
+    let titleFont = UIFont.systemFont(ofSize: 12.0, weight: .regular)
     // 2
     let titleAttributes: [NSAttributedString.Key: Any] =
       [NSAttributedString.Key.font: titleFont]
@@ -67,8 +67,8 @@ class PDFCreator: NSObject {
     // 3
     let titleStringSize = attributedTitle.size()
     // 4
-    let titleStringRect = CGRect(x: 20,
-                                 y: 36, width: titleStringSize.width,
+    let titleStringRect = CGRect(x: 72,
+                                 y: 72, width: titleStringSize.width,
                                  height: titleStringSize.height)
     // 5
     attributedTitle.draw(in: titleStringRect)
@@ -87,7 +87,7 @@ class PDFCreator: NSObject {
     // 3
     let titleStringSize = attributedTitle.size()
     // 4
-    let titleStringRect = CGRect(x: 20,
+    let titleStringRect = CGRect(x: 72,
                                  y: titleTop, width: titleStringSize.width,
                                  height: titleStringSize.height)
     // 5
@@ -111,7 +111,7 @@ class PDFCreator: NSObject {
     ]
     let attributedText = NSAttributedString(string: body, attributes: textAttributes)
     // 4
-    let textRect = CGRect(x: 10, y: textTop, width: pageRect.width - 20,
+    let textRect = CGRect(x: 72, y: textTop, width: pageRect.width - 144,
                           height: pageRect.height - textTop - pageRect.height / 5.0)
     attributedText.draw(in: textRect)
   }
@@ -128,66 +128,67 @@ class PDFCreator: NSObject {
     let scaledWidth = image.size.width * aspectRatio
     let scaledHeight = image.size.height * aspectRatio
     // 4
-    let imageX = (pageRect.width - scaledWidth) / 2.0
-    let imageRect = CGRect(x: imageX, y: imageTop,
-                           width: scaledWidth, height: scaledHeight)
+//    let imageX = (pageRect.width - scaledWidth) / 2.0
+    let imageX = 100.0
+    let imageRect = CGRect(x: 72, y: imageTop,
+                           width: 100.0, height: 50.0)
     // 5
     image.draw(in: imageRect)
     return imageRect.origin.y + imageRect.size.height
   }
   
-  // 1
-  func drawTearOffs(_ drawContext: CGContext, pageRect: CGRect,
-                    tearOffY: CGFloat, numberTabs: Int) {
-    // 2
-    drawContext.saveGState()
-    drawContext.setLineWidth(2.0)
-    
-    // 3
-    drawContext.move(to: CGPoint(x: 0, y: tearOffY))
-    drawContext.addLine(to: CGPoint(x: pageRect.width, y: tearOffY))
-    drawContext.strokePath()
-    drawContext.restoreGState()
-    
-    // 4
-    drawContext.saveGState()
-    let dashLength = CGFloat(72.0 * 0.2)
-    drawContext.setLineDash(phase: 0, lengths: [dashLength, dashLength])
-    // 5
-    let tabWidth = pageRect.width / CGFloat(numberTabs)
-    for tearOffIndex in 1..<numberTabs {
-      // 6
-      let tabX = CGFloat(tearOffIndex) * tabWidth
-      drawContext.move(to: CGPoint(x: tabX, y: tearOffY))
-      drawContext.addLine(to: CGPoint(x: tabX, y: pageRect.height))
-      drawContext.strokePath()
-    }
-    // 7
-    drawContext.restoreGState()
-  }
-  
-  func drawContactLabels(_ drawContext: CGContext, pageRect: CGRect, numberTabs: Int) {
-    let contactTextFont = UIFont.systemFont(ofSize: 10.0, weight: .regular)
-    let paragraphStyle = NSMutableParagraphStyle()
-    paragraphStyle.alignment = .natural
-    paragraphStyle.lineBreakMode = .byWordWrapping
-    let contactBlurbAttributes = [
-      NSAttributedString.Key.paragraphStyle: paragraphStyle,
-      NSAttributedString.Key.font: contactTextFont
-    ]
-    let attributedContactText = NSMutableAttributedString(string: contactInfo, attributes: contactBlurbAttributes)
-    // 1
-    let textHeight = attributedContactText.size().height
-    let tabWidth = pageRect.width / CGFloat(numberTabs)
-    let horizontalOffset = (tabWidth - textHeight) / 2.0
-    drawContext.saveGState()
-    // 2
-    drawContext.rotate(by: -90.0 * CGFloat.pi / 180.0)
-    for tearOffIndex in 0...numberTabs {
-      let tabX = CGFloat(tearOffIndex) * tabWidth + horizontalOffset
-      // 3
-      attributedContactText.draw(at: CGPoint(x: -pageRect.height + 5.0, y: tabX))
-    }
-    drawContext.restoreGState()
-  }
+//  // 1
+//  func drawTearOffs(_ drawContext: CGContext, pageRect: CGRect,
+//                    tearOffY: CGFloat, numberTabs: Int) {
+//    // 2
+//    drawContext.saveGState()
+//    drawContext.setLineWidth(2.0)
+//
+//    // 3
+//    drawContext.move(to: CGPoint(x: 0, y: tearOffY))
+//    drawContext.addLine(to: CGPoint(x: pageRect.width, y: tearOffY))
+//    drawContext.strokePath()
+//    drawContext.restoreGState()
+//
+//    // 4
+//    drawContext.saveGState()
+//    let dashLength = CGFloat(72.0 * 0.2)
+//    drawContext.setLineDash(phase: 0, lengths: [dashLength, dashLength])
+//    // 5
+//    let tabWidth = pageRect.width / CGFloat(numberTabs)
+//    for tearOffIndex in 1..<numberTabs {
+//      // 6
+//      let tabX = CGFloat(tearOffIndex) * tabWidth
+//      drawContext.move(to: CGPoint(x: tabX, y: tearOffY))
+//      drawContext.addLine(to: CGPoint(x: tabX, y: pageRect.height))
+//      drawContext.strokePath()
+//    }
+//    // 7
+//    drawContext.restoreGState()
+//  }
+//
+//  func drawContactLabels(_ drawContext: CGContext, pageRect: CGRect, numberTabs: Int) {
+//    let contactTextFont = UIFont.systemFont(ofSize: 10.0, weight: .regular)
+//    let paragraphStyle = NSMutableParagraphStyle()
+//    paragraphStyle.alignment = .natural
+//    paragraphStyle.lineBreakMode = .byWordWrapping
+//    let contactBlurbAttributes = [
+//      NSAttributedString.Key.paragraphStyle: paragraphStyle,
+//      NSAttributedString.Key.font: contactTextFont
+//    ]
+//    let attributedContactText = NSMutableAttributedString(string: contactInfo, attributes: contactBlurbAttributes)
+//    // 1
+//    let textHeight = attributedContactText.size().height
+//    let tabWidth = pageRect.width / CGFloat(numberTabs)
+//    let horizontalOffset = (tabWidth - textHeight) / 2.0
+//    drawContext.saveGState()
+//    // 2
+//    drawContext.rotate(by: -90.0 * CGFloat.pi / 180.0)
+//    for tearOffIndex in 0...numberTabs {
+//      let tabX = CGFloat(tearOffIndex) * tabWidth + horizontalOffset
+//      // 3
+//      attributedContactText.draw(at: CGPoint(x: -pageRect.height + 5.0, y: tabX))
+//    }
+//    drawContext.restoreGState()
+//  }
 }
